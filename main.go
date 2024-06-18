07f362820b69983f396dee3de2e59efaeb420449
GoogleGeek <ffmpeg@gmail.com>
2024-06-18 23:20:36 +0800

    support DNS query by UDP over Socks5

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"container/list"
+	"encoding/binary"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/miekg/dns"
 	"github.com/sirupsen/logrus"
@@ -134,21 +137,8 @@ func extractAnswerSection(response *dns.Msg) string {
 	return answerSection
 }
 
-// QueryDNS queries DNS through a SOCKS5 proxy using the specified protocol (TCP or UDP)
-func QueryDNS(domain string, config ProxyConfig, upDNS string) (*dns.Msg, error) {
-	// Create a SOCKS5 dialer
-	proxyAddr := net.JoinHostPort(config.Server, strconv.Itoa(config.Port))
-	auth := &proxy.Auth{
-		User:     config.Username,
-		Password: config.Password,
-	}
-	dialer, err := proxy.SOCKS5(config.Protocol, proxyAddr, auth, proxy.Direct)
-	if err != nil {
-		log.Errorf("Failed to create SOCKS5 dialer: %v", err)
-		return nil, err
-	}
-
-	// Create DNS query
+// queryTCP handles DNS queries over TCP
+func queryTCP(domain string, dialer proxy.Dialer, upDNS string) (*dns.Msg, error) {
 	msg := new(dns.Msg)
 	msg.SetQuestion(dns.Fqdn(domain), dns.TypeA)
 	buf, err := msg.Pack()
@@ -163,11 +153,8 @@ func QueryDNS(domain string, config ProxyConfig, upDNS string) (*dns.Msg, error)
 	tcpBuf[1] = byte(len(buf))
 	copy(tcpBuf[2:], buf)
 
-	// Log the packed DNS query
-	log.Debugf("Packed DNS query (with length): %x", tcpBuf)
-
 	// Create a unique connection for this query
-	conn, err := dialer.Dial(config.Protocol, upDNS)
+	conn, err := dialer.Dial("tcp", upDNS)
 	if err != nil {
 		log.Errorf("Failed to dial to upstream DNS server: %v", err)
 		return nil, err
@@ -175,7 +162,7 @@ func QueryDNS(domain string, config ProxyConfig, upDNS string) (*dns.Msg, error)
 	defer conn.Close()
 
 	// Send DNS query
-	log.Infof("Sending DNS query for domain: %s using protocol: %s", domain, config.Protocol)
+	log.Debugf("Sending DNS query for domain: %s using protocol: tcp", domain)
 	if _, err := conn.Write(tcpBuf); err != nil {
 		log.Errorf("Failed to write to connection: %v", err)
 		return nil, err
@@ -205,6 +192,233 @@ func QueryDNS(domain string, config ProxyConfig, upDNS string) (*dns.Msg, error)
 	log.Infof("DNS query for domain: %s succeeded. Response: %v", domain, extractAnswerSection(response))
 
 	return response, nil
+}
+
+// socks5UDPAssociate performs a UDP ASSOCIATE request to the SOCKS5 proxy
+func socks5UDPAssociate(proxyAddr, username, password string) (*net.UDPConn, net.Conn, error) {
+	conn, err := net.Dial("tcp", proxyAddr)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to connect to proxy: %v", err)
+	}
+
+	log.Debugf("Connected to SOCKS5 proxy at %s", proxyAddr)
+
+	// SOCKS5 handshake with username/password authentication
+	if username != "" && password != "" {
+		// Initial greeting
+		_, err = conn.Write([]byte{0x05, 0x01, 0x02}) // SOCKS5, 1 auth method, username/password
+		if err != nil {
+			conn.Close()
+			return nil, nil, fmt.Errorf("failed to write initial handshake: %v", err)
+		}
+
+		// Read greeting response
+		response := make([]byte, 2)
+		_, err = conn.Read(response)
+		if err != nil {
+			conn.Close()
+			return nil, nil, fmt.Errorf("failed to read initial handshake response: %v", err)
+		}
+
+		if response[0] != 0x05 || response[1] != 0x02 {
+			conn.Close()
+			return nil, nil, fmt.Errorf("unexpected initial handshake response: %v", response)
+		}
+
+		// Username/Password authentication
+		authMsg := []byte{0x01, byte(len(username))}
+		authMsg = append(authMsg, []byte(username)...)
+		authMsg = append(authMsg, byte(len(password)))
+		authMsg = append(authMsg, []byte(password)...)
+
+		_, err = conn.Write(authMsg)
+		if err != nil {
+			conn.Close()
+			return nil, nil, fmt.Errorf("failed to write auth message: %v", err)
+		}
+
+		// Read auth response
+		authResponse := make([]byte, 2)
+		_, err = conn.Read(authResponse)
+		if err != nil {
+			conn.Close()
+			return nil, nil, fmt.Errorf("failed to read auth response: %v", err)
+		}
+
+		if authResponse[0] != 0x01 || authResponse[1] != 0x00 {
+			conn.Close()
+			return nil, nil, fmt.Errorf("auth failed: %v", authResponse)
+		}
+	} else {
+		// Initial greeting without authentication
+		_, err = conn.Write([]byte{0x05, 0x01, 0x00}) // SOCKS5, 1 auth method, no auth
+		if err != nil {
+			conn.Close()
+			return nil, nil, fmt.Errorf("failed to write initial handshake: %v", err)
+		}
+
+		// Read greeting response
+		response := make([]byte, 2)
+		_, err = conn.Read(response)
+		if err != nil {
+			conn.Close()
+			return nil, nil, fmt.Errorf("failed to read initial handshake response: %v", err)
+		}
+
+		if response[0] != 0x05 || response[1] != 0x00 {
+			conn.Close()
+			return nil, nil, fmt.Errorf("unexpected initial handshake response: %v", response)
+		}
+	}
+
+	log.Debugf("SOCKS5 handshake completed")
+
+	// Send UDP ASSOCIATE request
+	udpRequest := []byte{
+		0x05, 0x03, 0x00, 0x01, // SOCKS5, UDP ASSOCIATE, reserved, IPv4
+		0x00, 0x00, 0x00, 0x00, // 0.0.0.0 (address)
+		0x00, 0x00, // 0 (port)
+	}
+	_, err = conn.Write(udpRequest)
+	if err != nil {
+		conn.Close()
+		return nil, nil, fmt.Errorf("failed to send UDP ASSOCIATE request: %v", err)
+	}
+
+	log.Debugf("UDP ASSOCIATE request sent")
+
+	response := make([]byte, 10)
+	_, err = conn.Read(response)
+	if err != nil {
+		conn.Close()
+		return nil, nil, fmt.Errorf("failed to read UDP ASSOCIATE response: %v", err)
+	}
+
+	log.Debugf("UDP ASSOCIATE response received: %v", response)
+
+	if response[1] != 0x00 {
+		conn.Close()
+		return nil, nil, fmt.Errorf("UDP ASSOCIATE request failed: %v", response[1])
+	}
+
+	proxyIP := net.IPv4(response[4], response[5], response[6], response[7])
+	proxyPort := binary.BigEndian.Uint16(response[8:10])
+	proxyUDPAddr := &net.UDPAddr{
+		IP:   proxyIP,
+		Port: int(proxyPort),
+	}
+
+	log.Debugf("Proxy UDP address: %v", proxyUDPAddr)
+
+	udpConn, err := net.DialUDP("udp", nil, proxyUDPAddr)
+	if err != nil {
+		conn.Close()
+		return nil, nil, fmt.Errorf("failed to dial UDP: %v", err)
+	}
+
+	return udpConn, conn, nil
+}
+
+// queryUDP handles DNS queries over UDP
+func queryUDP(domain string, proxyConfig ProxyConfig, upDNS string) (*dns.Msg, error) {
+	proxyAddr := net.JoinHostPort(proxyConfig.Server, strconv.Itoa(proxyConfig.Port))
+	udpConn, tcpConn, err := socks5UDPAssociate(proxyAddr, proxyConfig.Username, proxyConfig.Password)
+	if err != nil {
+		log.Errorf("UDP ASSOCIATE failed: %v", err)
+		return nil, err
+	}
+	defer udpConn.Close()
+	defer tcpConn.Close() // Close the TCP connection after UDP communication is done
+
+	// Create DNS query
+	msg := new(dns.Msg)
+	msg.SetQuestion(dns.Fqdn(domain), dns.TypeA)
+	buf, err := msg.Pack()
+	if err != nil {
+		log.Errorf("Failed to pack DNS query: %v", err)
+		return nil, err
+	}
+
+	targetAddr, err := net.ResolveUDPAddr("udp", upDNS)
+	if err != nil {
+		log.Errorf("Failed to resolve target address: %v", err)
+		return nil, err
+	}
+
+	log.Debugf("Resolved target address: %v", targetAddr)
+
+	// Prepare the UDP packet
+	var udpBuf bytes.Buffer
+	udpBuf.WriteByte(0x00) // RSV
+	udpBuf.WriteByte(0x00) // RSV
+	udpBuf.WriteByte(0x00) // FRAG
+	udpBuf.WriteByte(0x01) // ATYP (IPv4)
+	udpBuf.Write(targetAddr.IP.To4())
+	binary.Write(&udpBuf, binary.BigEndian, uint16(targetAddr.Port))
+	udpBuf.Write(buf)
+
+	log.Debugf("Constructed UDP packet data: %x", udpBuf.Bytes())
+
+	// Send UDP packet to SOCKS5 proxy server
+	log.Debugf("Sending UDP packet to proxy server")
+	if _, err := udpConn.Write(udpBuf.Bytes()); err != nil {
+		log.Errorf("Failed to send UDP data: %v", err)
+		return nil, err
+	}
+
+	// Set read deadline
+	udpConn.SetReadDeadline(time.Now().Add(5 * time.Second))
+
+	// Receive response
+	responseBuf := make([]byte, 1024)
+	n, addr, err := udpConn.ReadFrom(responseBuf)
+	if err != nil {
+		log.Errorf("Failed to read UDP response: %v", err)
+		return nil, err
+	}
+
+	log.Debugf("Received UDP response from %v, data: %x", addr, responseBuf[:n])
+
+	// Remove the SOCKS5 UDP header
+	if n < 10 {
+		return nil, fmt.Errorf("invalid UDP response length")
+	}
+	udpResponseData := responseBuf[10:n]
+
+	// Unpack the response
+	response := new(dns.Msg)
+	if err := response.Unpack(udpResponseData); err != nil { // Use the correct length
+		log.Errorf("Failed to unpack DNS response: %v", err)
+		log.Errorf("UDP response data: %x", udpResponseData) // Log the response data for debugging
+		return nil, err
+	}
+
+	log.Infof("DNS query for domain: %s succeeded. Response: %v", domain, extractAnswerSection(response))
+
+	return response, nil
+}
+
+// QueryDNS queries DNS through a SOCKS5 proxy using the specified protocol (TCP or UDP)
+func QueryDNS(domain string, config ProxyConfig, upDNS string) (*dns.Msg, error) {
+	// Create a SOCKS5 dialer
+	proxyAddr := net.JoinHostPort(config.Server, strconv.Itoa(config.Port))
+	auth := &proxy.Auth{
+		User:     config.Username,
+		Password: config.Password,
+	}
+	dialer, err := proxy.SOCKS5("tcp", proxyAddr, auth, proxy.Direct)
+	if err != nil {
+		log.Errorf("Failed to create SOCKS5 dialer: %v", err)
+		return nil, err
+	}
+
+	if config.Protocol == "tcp" {
+		return queryTCP(domain, dialer, upDNS)
+	} else if config.Protocol == "udp" {
+		return queryUDP(domain, config, upDNS)
+	}
+
+	return nil, fmt.Errorf("Unsupported protocol: %s", config.Protocol)
 }
 
 // DNSHandler handles DNS requests
@@ -266,7 +480,7 @@ func DNSHandler(w dns.ResponseWriter, req *dns.Msg) {
 	upDNS := upstreamDNSConfig.UpDNS
 	configDnsMu.RUnlock()
 
-	log.Infof("Received DNS query for domain: %s from source IP: %s", domain, sourceIP)
+	log.Debugf("Received DNS query for domain: %s from source IP: %s", domain, sourceIP)
 
 	response, err := QueryDNS(domain, config, upDNS)
 	if err != nil {
@@ -280,7 +494,7 @@ func DNSHandler(w dns.ResponseWriter, req *dns.Msg) {
 	cache.Put(domain, response)
 
 	m.Answer = response.Answer
-	log.Infof("Writing DNS response for domain: %s", domain)
+	log.Debugf("Writing DNS response for domain: %s", domain)
 	if err := w.WriteMsg(m); err != nil {
 		log.Errorf("Failed to write DNS response: %v", err)
 	}
